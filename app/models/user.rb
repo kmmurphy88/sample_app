084b3daba73e3e80b9ae48315f3d8dc9b483f734
Kevin Murphy <km86463@gmail.com>
2014-12-15 13:36:25 -0700

    In progress on setting up posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token
   before_save { email.downcase! }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
